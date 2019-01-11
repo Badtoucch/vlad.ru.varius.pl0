893693e02f85a697b3617e96c805292f0409c574
Vladislad T <v.trundenko@everad.com>
2019-01-11 14:06:02 +0200

    vlad

--- README.md
+++
@@ -1,2 +0,0 @@
-# vlad.ru.varius.pl0
-# vlad.ru.varius.pl0
---
+++ gulp/paths/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = [
+  './source/js/app.js',
+  './source/js/slick.min.js',
+  './source/js/jquery-3.3.1.min.js',
+  './source/js/slick.min.js',
+   './source/js/everad.js'
+];
